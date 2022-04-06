a19d6990e8331b0a847bd028e99cb73decb22add
Evan Greco <egreco12@github.com>
2022-04-06 23:56:28 +0000

    Add function for fetching json file

--- isthecatwinning-fe/src/app/score.service.ts
+++ isthecatwinning-fe/src/app/score.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Player, MOCK_PLAYERS } from './players';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScoreService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.getJSON().subscribe(data => {
+      console.log(data);
+    });
+  }
 
   getPlayers(): Player[] {
     return MOCK_PLAYERS;
-}
+  }
+
+  getJSON(): Observable<any> {
+    return this.http.get("./assets/players.json");
+  }
 }
